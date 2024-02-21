3293d6c9c9449b5ed80f665d62295d243c1fa38b
geoffroy de cooman <geoffroy.decooman@gmail.com>
2024-02-21 10:58:47 +0100

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,23 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # read all
+  get "tasks", to: "tasks#index"
+
+  # create
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # read 1
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # update
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete
+  delete "task/:id", to: "tasks#destroy", as: :task_destroy
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
